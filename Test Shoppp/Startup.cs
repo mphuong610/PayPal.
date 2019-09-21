f593bab0b6b166706057b086772672249430dc87
Administrator <Administrator@PC426>
2019-09-21 15:58:25 +0700

    Add project files.

---
+++ Test Shoppp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Shoppp.Startup))]
+namespace Test_Shoppp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
